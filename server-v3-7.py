9279fb0efc6e111f3079b98601d88743505b72f9
German Grandas <gertiven@gmail.com>
2018-08-30 21:48:04 -0500

    update ip

--- server-v3-7.py
+++ server-v3-7.py
@@ -3,7 +3,7 @@ from xmlrpc.server import SimpleXMLRPCServer
 def	suma_remota(a,b):
 	return a+b
 	
-server = SimpleXMLRPCServer(("localhost", 8001))
+server = SimpleXMLRPCServer(("40.74.231.113", 8001))
 server.register_function(suma_remota, "suma")
 print ("soy el servidor y estoy corriendo por el pueto 8001")
 server.serve_forever()
