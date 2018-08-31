84d3b3f44c7dfb79e29ce5c37cb9004cfb53f86b
German Grandas <gertiven@gmail.com>
2018-08-30 21:42:45 -0500

    testing server

---
+++ server-v3-7.py
@@ -0,0 +1,9 @@
+from xmlrpc.server import SimpleXMLRPCServer
+
+def	suma_remota(a,b):
+	return a+b
+	
+server = SimpleXMLRPCServer(("localhost", 8001))
+server.register_function(suma_remota, "suma")
+print ("soy el servidor y estoy corriendo por el pueto 8001")
+server.serve_forever()
